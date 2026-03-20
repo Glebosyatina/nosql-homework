b64be3249f9282127da41c7c278cce740d169a72
Gleb Pivushev <gpivusev@gmail.com>
2026-03-20 17:18:53 +0300

    rate limiter теперь работает не через fixedWindow, а через slidingWindow:)

--- redis/src/main/java/ratelimiter/RateLimiter.java
+++ redis/src/main/java/ratelimiter/RateLimiter.java
@@ -4,6 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.Instant;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.args.ExpiryOption;
@@ -26,6 +32,7 @@ public class RateLimiter {
     // TODO: Implementation
     //rate limiter на основе fixed window, через счетчик контролируем запросы в рамках одного окна
     // по прошествию окна сбрасываем счетчик, решение со slidingWindow для последнего теста пока не додумал
+    /*
     String strCount = redis.get(label);   //достаем сколько запросов сделано
     int count = strCount != null ? Integer.parseInt(strCount) : 0;
 
@@ -35,6 +42,48 @@ public class RateLimiter {
       return true;
     }
     return false;
+     */
+
+    //подход такой, на стороне редис будем хранить сет со временем запросов
+    //и так же выполнять логику по "сдвигу" окна, то есть уменьшению колва запросов в текущем окне
+    //как погуглил лучше это делать через lua скрипт
+    //через KEY и ARGV передаем в редис, запрос(название сета), окно, макс.колво запросов, текущее время
+    //ZREMRANGEBYSCORE удаляет все значения в сете не принадлещацие диапазону(время которых уже прошло и осталось за окном)
+    //ZCARD количество запросов в текущем окне
+    //ZADD для добавления запроса с его временем
+    //EXPIRE удаляем набор запросов по если в течении всего окна не было запросов
+    String luaScript = """
+        local key = KEYS[1]
+        local window = tonumber(ARGV[1])
+        local limit = tonumber(ARGV[2])
+        local now = tonumber(ARGV[3])
+        
+        redis.call('ZREMRANGEBYSCORE', key, 0, now - window)
+        local count = redis.call('ZCARD', key)
+        
+        if count < limit then
+            local micro = redis.call('TIME')[2]
+            local member = now .. ':' .. micro
+            redis.call('ZADD', key, now, member)
+            redis.call('EXPIRE', key, window)
+            return 1
+        else
+            return 0
+        end
+        """;
+
+    long now = System.currentTimeMillis() / 1000;
+
+    List<String> key = Arrays.asList(label); //наш запрос
+    List<String> args = Arrays.asList(        //в виде args передаем параметры
+        String.valueOf(timeWindowSeconds),
+        String.valueOf(maxRequestCount),
+        String.valueOf(now)
+    );
+
+    Long result = (Long) redis.eval(luaScript, key, args); //оказывается протокол redis не поддерживает boolean
+    return result == 1;
+    //падает на requestWithInsufficientIntervalsTest тесте так как там не работает логика проверки со сдвигающимся окном
   }
 
   public static void main(String[] args) {
